137ae9f919b4ac030fce220d174995f735ee55b7
JowiSK <jnmartenss@gmail.com>
2024-02-26 17:26:38 +0000

    removed unnecessary files

--- config.sample.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-      host: 'localhost',
-      user: 'jowi',
-      password: 'jowi',
-      database: 'noteus',
-    },
-    api_key: 'postgres://hapxwbcozesmot:b8fe817669e8734cd3e598e45ed710b1e168476060b054c0e73706a93b10df95@ec2-34-236-199-229.compute-1.amazonaws.com:5432/d57huseftfqq9k',
-  };
