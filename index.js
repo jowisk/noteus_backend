d33dedaf21739fcd4fea9b1b66a44413716f682c
JowiSK <jnmartenss@gmail.com>
2024-02-26 17:46:30 +0000

    add test log

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+
 
 const notesRouter = require('./routes/notes')
 const authRouter = require('./routes/auth')
