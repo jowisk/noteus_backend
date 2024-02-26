44f0dead6b40c6dba6780f666123aa9a0193f919
JowiSK <jnmartenss@gmail.com>
2024-02-26 17:13:55 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+const pgp = require('pg-promise')();
+
+
+const notesRouter = require('./routes/notes')
+const authRouter = require('./routes/auth')
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors("https://localhost:5173"));
+
+app.use("/notes", notesRouter);
+app.use("/auth", authRouter);
+
+
+app.listen(4000, () => {
+  console.log("Server on");
+});
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: No token provided' });
+  }
+
+  jwt.verify(token, TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden: Invalid token' });
+    }
+    req.user = user; // Attach the user information to the request
+    next();
+  });
+};
+
+module.exports = { 
+  authenticateToken,
+  TOKEN_SECRET};
